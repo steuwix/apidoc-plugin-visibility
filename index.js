8d99b45dc9b325ddf1eebf28f61005d94d686508
steuwix <steevy.bret@gmail.com>
2017-04-03 18:16:35 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+var app = {};
+
+module.exports = {
+  init: function(_app) {
+    app = _app;
+
+    //app.addHook('parser-find-elements', parserFindVisibilityApis);
+
+    app.parsers.apivisibility = {
+      parse     : parseVisibility,
+      path      : 'local',
+      method    : 'insert'
+    };
+  }
+
+};
+
+function parseVisibility(content) {
+  var list = content.split(" ");
+  var result = content;
+  if(list.length > 1){
+      result = list;
+  }
+  return {
+    visibility : result
+  };
+}
